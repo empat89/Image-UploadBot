699274105883fc41caafa3cde30bf54414be190a
empat89 <102323245+empat89@users.noreply.github.com>
2022-06-10 14:05:19 +0700

    Add files via upload

---
+++ push.sh
@@ -0,0 +1,5 @@
+apt get install && apt get upgrade
+curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-389.0.0-linux-x86_64.tar.gz
+tar -xf google-cloud-cli-389.0.0-linux-x86.tar.gz
+./google-cloud-sdk/install.sh
+./google-cloud-sdk/bin/gcloud init
